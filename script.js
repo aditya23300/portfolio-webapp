fdca52a69efaa7fffc5b2a46a917e5cc8db68ee3
Aditya Raj <88247211+aditya23300@users.noreply.github.com>
2024-11-21 03:35:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Example: Add an animation to alert when clicking on the Resume button
-const resumeBtn = document.querySelector(".btn");
-resumeBtn.addEventListener("click", () => {
-  Swal.fire({
-    title: "Downloading...",
-    text: "Your resume is downloading!",
-    icon: "success",
-    confirmButtonText: "Great!",
-  });
-});
+
